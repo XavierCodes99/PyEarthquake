647b1cf323638b06741018e5297d7cd000ab113e
XavierCodes99 <88925152+XavierCodes99@users.noreply.github.com>
2023-03-23 15:02:32 +0800

    1.0.1.3 Making it faster for low-end computers

--- main.py
+++ main.py
@@ -4,30 +4,24 @@ import numpy as np
 from mpl_toolkits.basemap import Basemap
 from matplotlib.animation import FuncAnimation
 from matplotlib.colors import ListedColormap
-import pyttsx3
-import datetime
-
-engine = pyttsx3.init()
 
 # Define the colormap
-colormap = ListedColormap(['#ccc', '#0ff', '#00f', '#0f0', '#af0', '#ff0', '#fa0', '#f00', '#a00', '#d0f'])
+colormap = ListedColormap(['#ccc', '#0ff', '#00f', '#0f0', '#af0', '#ff0', '#fa0', '#f00', '#a00', '#a0f'])
 
 # Initialize the plot
 fig = plt.figure(figsize=(10, 7))
 ax = fig.add_subplot(111)
 
-m = Basemap(projection='merc', llcrnrlon=-180, llcrnrlat=-80, urcrnrlon=180, urcrnrlat=80, resolution='i')
+m = Basemap(projection='merc', llcrnrlon=-180, llcrnrlat=-80, urcrnrlon=180, urcrnrlat=80, resolution='l')
 
 m.drawcoastlines()
 m.drawcountries()
-m.drawmapboundary(fill_color='#aff')
-m.fillcontinents(color='#eee')
 
 # Initialize the scatter plot
-sc = ax.scatter([], [], [], alpha=1, cmap=colormap, edgecolor='black', zorder=2)
+sc = ax.scatter([], [], [], alpha=0.75, cmap=colormap, edgecolor='black', zorder=2, marker="o")
 
 # Add a colorbar
-cbar = plt.colorbar(sc, label='Magnitude', orientation='horizontal')
+cbar = plt.colorbar(sc, label='Magnitude')
 
 # Set the x and y limits
 ax.set_xlim(m.llcrnrx, m.urcrnrx)
@@ -53,27 +47,10 @@ def update_plot(i):
     sc.set_offsets(list(zip(x, y)))
     sc.set_array(mag)
     sc.set_cmap(colormap)
-    sc.set_sizes(np.exp(mag) / 3)
-    sc.set_clim(vmin=0, vmax=9)
+    sc.set_sizes(np.exp(mag))
+    sc.set_clim(vmin=1, vmax=9)
     
     fig.canvas.manager.set_window_title('PyEarthquake')
-    
-        # Check if a new earthquake has occurred
-    if len(df) > len(lat):
-        # Get the latest earthquake data
-        latest = df.iloc[-1]
-        mag = latest['mag']
-        place = latest['place']
-        time = latest['time']
-        
-        date_obj = datetime.strptime(str(time), '%Y-%m-%dT%H:%M:%S.%fZ')
-        
-        newtime = date_obj.strftime('%H:%M %p')
-        
-        # Speak the earthquake information
-        text = f"An earthquake with a magnitude of {mag} has occured in {place} at {newtime}"
-        engine.say(text)
-        engine.runAndWait()
 
 # Animate the plot
 ani = FuncAnimation(fig, update_plot, interval=1000)
